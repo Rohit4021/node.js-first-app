796ae4c95f9f9ade36159f20b4aaffb040c227c8
Rohit4021 <87359774+Rohit4021@users.noreply.github.com>
2022-07-01 16:43:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ const server = app.listen(process.env.PORT || 8080, async function() {
     const ProjectUuid = 'ec2d5337'
     const body = 'Hi, I am Rohit'
     const voiceUuid = '14ebc696'
-    const callback_uri = 'https://pokemonxyz.herokuapp.com/'
 
     await resemble.clips.createAsync(ProjectUuid, {
         body: body,
         voice: voiceUuid,
-        callback_uri: callback_uri
+        callback_uri: 'https://pokemonxyz.herokuapp.com/'
     })
 })
