375e3786f5e70e2fb03e5638dbea1b0d452bad6e
Rohit4021 <87359774+Rohit4021@users.noreply.github.com>
2022-07-03 15:00:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const Resemble = require('@resemble/node')
+const fetch = require("node-fetch");
+const {response} = require("express");
 
 const server = app.listen(process.env.PORT || 8080, async function() {
     const resemble = new Resemble('v2', 'S9uIzqSGjQ19i2BqWCExhAtt')
-    const ProjectUuid = 'ec2d5337'
+    const title = "New Clip"
     const body = 'Hi, I am Rohit'
-    const voiceUuid = '14ebc696'
+    const voice_uuid = '14ebc696'
+    const is_public = true
 
-    await resemble.clips.createAsync(ProjectUuid, {
-        body: body,
-        voice: voiceUuid,
-        callback_uri: 'https://pokemonxyz.herokuapp.com/'
+    const data = {title, body, voice_uuid, is_public}
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    fetch('https://app.resemble.ai/api/v2/projects/ec2d5337/clips', options).then(response=> {
+        console.log(response)
     })
 })
