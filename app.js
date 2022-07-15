ef0e44159fee17e124865f79c4157a4425717477
Rohit4021 <87359774+Rohit4021@users.noreply.github.com>
2022-07-15 21:34:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,38 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const Resemble = require('@resemble/node')
-const fetch = require('node-fetch')
-const {response} = require('express')
-const Apify = require('apify')
-const { log } = Apify.utils
-
-const INPUT = {
-    "username": "rohitkm4021@gmail.com",
-    "password": "*i*am*TIND*#1"
+{
+  "name": "projects",
+  "version": "1.0.0",
+  "description": "",
+  "main": "app.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "app.js"
+  },
+  "engines": {
+    "node": "16.x",
+    "npm": "6.x"
+  },
+  "dependencies": {
+    "form-data": "^4.0.0",
+    "node-fetch": "^2.6.1"
+  },
+  "devDependencies": {
+    "express": "^4.18.1",
+    "@resemble/node": "^1.2.1",
+    "@types/node": "^18.0.0",
+    "@types/jest": "^28.1.3",
+    "@types/mocha": "^9.1.1",
+    "mocha": "^7.0.2",
+    "nodemon": "^2.0.9",
+    "nyc": "^15.1.0",
+    "ts-mocha": "^8.0.0",
+    "typescript": "^4.3.5",
+    "typo-js": "^1.2.1",
+    "apify": "^2.3.2",
+    "puppeteer": "9.x",
+    "request-promise": "^4.2.6",
+    "request": "^2.88.2"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
 }
-
-await INPUT.json()
-
-Apify.main(async () => {
-    // Get the username and password inputs
-    const input = await Apify.getValue('INPUT')
-
-    const browser = await Apify.launchPuppeteer()
-    const page = await browser.newPage()
-    await page.goto('https://app.resemble.ai/users/sign_in')
-
-    //Login
-    await page.type('#user_email')
-})
-
-const server = app.listen(process.env.PORT || 8080, async function() {
-    //const resemble = new Resemble('v2', 'S9uIzqSGjQ19i2BqWCExhAtt')
-    const title = "New Clip"
-    const body = 'Hi, I am Rohit'
-    const voice_uuid = '14ebc696'
-    const is_public = true
-
-    const data = {title, body, voice_uuid, is_public}
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }
-    fetch('https://app.resemble.ai/api/v2/projects/ec2d5337/clips', options).then(response => {
-        console.log(response)
-    })
-})
