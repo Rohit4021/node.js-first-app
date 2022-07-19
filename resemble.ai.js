aced2318c4cbce19d5b7ac32a03cafbe58587f2b
Rohit4021 <87359774+Rohit4021@users.noreply.github.com>
2022-07-19 19:46:24 +0530

    Update resemble.ai.js

--- resemble.ai.js
+++ resemble.ai.js
@@ -2,14 +2,21 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const Resemble = require('@resemble/node')
+const path = require('path')
 
-async function get(request, response) {
-    const resemble = new Resemble('v2', 'nr37R4EtojghW8SE8Kloowtt')
-    const projectUuid = "e3b47f1a"
-    const clipUuid = "8c3147b5"
+app.listen(5000, async function () {
+    const resemble = new Resemble('v2', 'Yij7R3EN5zisTHQBkWV3owtt')
+    const projectUuid = "5a7ed851"
+    const clipUuid = "ff5ba882"
 
     const get = await resemble.clips.get(projectUuid, clipUuid)
-    console.log(get)
-}
+    const convertString = JSON.stringify(get)
+    const obj = JSON.parse(convertString)
+    const audioSource = obj["item"]["audio_src"]
 
-app.listen(5000, get)
+    return audioSource
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
